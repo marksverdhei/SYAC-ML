b489d045f9a4b3b308283819e0cc84d1f710b470
marksverdhei <marksverdhei@hotmail.com>
2022-04-27 22:33:45 +0200

    Update predict script

--- predict.py
+++ predict.py
@@ -15,35 +15,36 @@ def predict_from_config(config):
     pipeline = TitleAnsweringPipeline.from_config(config)
     pipeline.tokenizer.model_max_length = 512
 
-    dev_predictions, test_predictions = predict_on_datasets(
-        dev_set_path, test_set_path, pipeline
+    dev_predictions, test_predictions = predict_on_df(
+        dev_set_path, pipeline
     )
 
     if not os.path.exists(f"evaluation/dev/{model_name}"):
         os.mkdir(f"evaluation/dev/{model_name}")
 
+    dev_predictions.to_csv(f"evaluation/dev/{model_name}/predictions.csv")
+
+    test_predictions = predict_on_df(
+        test_set_path, pipeline
+    )
+
     if not os.path.exists(f"evaluation/test/{model_name}"):
         os.mkdir(f"evaluation/test/{model_name}")
 
-    dev_predictions.to_csv(f"evaluation/dev/{model_name}/predictions.csv")
     test_predictions.to_csv(f"evaluation/test/{model_name}/predictions.csv")
 
 
-def predict_on_datasets(dev_set_path, test_set_path, pipeline):
-    dev_set = pd.read_csv(dev_set_path, index_col=0)
-    test_set = pd.read_csv(test_set_path, index_col=0)
+def predict_on_df(eval_set_path, pipeline):
+    eval_set = pd.read_csv(eval_set_path, index_col=0)
 
     def predict_on_row(row):
         return pipeline(row.title, row.body)
 
-    dev_predictions = dev_set.progress_apply(predict_on_row, axis=1).rename(
-        pipeline.name
-    )
-    test_predictions = test_set.progress_apply(predict_on_row, axis=1).rename(
+    eval_predictions = eval_set.progress_apply(predict_on_row, axis=1).rename(
         pipeline.name
     )
 
-    return dev_predictions, test_predictions
+    return eval_predictions
 
 
 def main() -> None:
